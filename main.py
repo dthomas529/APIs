5fe1415fe203edd5a990210ea0852571c3784f1d
Dawn F. Thomas <dthomas529@gmail.com>
2024-02-24 17:58:20 -0500

    Update main.py
    Added routes, decorators, HTTP methods GET and POST

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
+#jsonify creates a json object which is the response
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/get-user/<user_id>')
+#decorator make the datain the route accesinle to the gunction
+@app.route("/get-user/<user_id>")
 def get_user(user_id):
     user_data = {
         'user_id': user_id,
         'name:': 'John Doe',
         'email': 'jdoe@example.com'
         }
-#def home():
-     #return 'Home'
 
-if app == '__main__':
+extra = request.args.get('extra')
+if extra:
+  user_data['extra'] = extra
+  
+  return jsonify(user_data), 200
+
+@app.route('/create-user', methods=['POST'])
+def create_user():
+  data = request.get.json()
+
+return jsonify(data), 201
+
+#sets up the Fkask server
+if __name__ == '__main__':
     app.run(debug=True)
-    
+  
+#app.run(host='0.0.0.0', port=8080)
 
+    
